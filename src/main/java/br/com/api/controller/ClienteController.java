a37a14ef5a486a712086534e29ce098bccae7adc
Ailton <tym.ailton@gmail.com>
2021-02-12 12:15:09 -0300

    criando a conexao com o banco

--- src/main/java/br/com/api/controller/ClienteController.java
+++ src/main/java/br/com/api/controller/ClienteController.java
@@ -1,5 +1,20 @@
 package br.com.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
+	
+	
+	
+	@GetMapping(value = "/")
+	public void buscar() {
+		
+		System.out.println("chamou");
+		
+	}
 
 }
