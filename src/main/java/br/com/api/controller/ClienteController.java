97ddeb777ecb2e1377fa39358f6857f5f268bb0b
Ailton <tym.ailton@gmail.com>
2021-02-12 13:23:03 -0300

    metodo de salvarCliente POST

--- src/main/java/br/com/api/controller/ClienteController.java
+++ src/main/java/br/com/api/controller/ClienteController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,15 @@ public class ClienteController {
 		return new ResponseEntity<Cliente>(cliente.get(), HttpStatus.OK);
 		
 		
+	}
+	
+	@PostMapping(value = "/",produces = "application/json")
+	public ResponseEntity<Cliente> salvarCliente(@RequestBody Cliente cliente){
+		
+		Cliente clienteSalvo = clienteRepository.save(cliente);
+		
+		
+		return new ResponseEntity<Cliente>(clienteSalvo, HttpStatus.CREATED);
 	}
 
 }
