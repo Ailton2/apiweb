201b1a50333da2452803149cbca69da037b6229d
Ailton <tym.ailton@gmail.com>
2021-02-15 09:52:01 -0300

    metodo scope carregarClientes no app js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,10 +3,31 @@ var appCliente = angular.module("appCliente",[]);
 
 //criaçao de contrllers
 
-appCliente.controller("indexController",function($scope){
+appCliente.controller("indexController",function($scope,$http){
 	
 	$scope.nome="Ailton"
+	$scope.sobrenome="Ferreira"	
+		
+	$scope.clientes=[]	
+		
+
+
+		// Simple GET request example:
+	$scope.carregarClientes=function(){
+	    $http({
+			method : 'GET',
+			url : 'http://localhost:8080/apiweb/clientes/'
+		}).then(function success(response) {
+			$scope.clientes= response.data
+		    console.log(response.status)
+		}, function error(response) {
+			
+			   console.log(response.status)
+		});		
+		
+	}
 	
+	$scope.carregarClientes();
 	
 })
 
