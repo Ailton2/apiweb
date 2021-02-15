f729bf0d675c262ba6bb865168af5cbe84300e58
Ailton <tym.ailton@gmail.com>
2021-02-15 15:57:49 -0300

    criando rotas de esdados e cidades

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,6 +5,8 @@ var appCliente = angular.module("appCliente",['ngRoute']);
 appCliente.config(function ($routeProvider) {
    $routeProvider
    .when("/clientes",{templateUrl:'view/cliente.html',controller:'clienteController'})
+   .when("/cidades",{templateUrl:'view/cidade.html',controller:'cidadeController'})
+   .when("/estados",{templateUrl:'view/estado.html',controller:'estadoController'})
 	.otherwise({rediretTo:'/'});
 });
 
