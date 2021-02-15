7ad776c0c12646055392489b23ec7710e754f6a9
Ailton <tym.ailton@gmail.com>
2021-02-14 21:54:37 -0300

    adicionando o app.js e controllers.js

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,8 @@
+//criaçao do mudulo principal da aplicaçao
+var appCliente = angular.module("appCliente",[]); 
+
+//criaçao de contrllers
+
+appCliente.controller("clienteController",function(){})
+
+
