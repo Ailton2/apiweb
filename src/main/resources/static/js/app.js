79c7e76789f6bf05b84e1862e41995f2c9cfb568
Ailton <tym.ailton@gmail.com>
2021-02-15 14:59:41 -0300

    criando o routerProvider do cliente

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,3 +2,9 @@
 var appCliente = angular.module("appCliente",['ngRoute']); 
 
 
+appCliente.config(function ($routeProvider) {
+   $routeProvider
+   .when("/clientes",{templateUrl:"view/cliente.html",controller:"clienteController"})
+	.otherwise({rediretTo:'/'})
+})
+
